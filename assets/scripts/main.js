1b950d5827db70dd95793c8020e850a2ab7b13f0
Renan Tiberto <renan.tiberto@gmail.com>
2025-06-06 21:48:42 -0300

    Fix: Update Current Year in Footer

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,10 +1,15 @@
-//Dark Theme - Light Theme;
-let checkbox = document.getElementById("checkbox");
+const currentDate = new Date()
+let year = currentDate.getFullYear()
 
-checkbox.addEventListener("change", function(){
-    if(this.checked){
-        document.documentElement.setAttribute("data-theme", "dark");
-    }else{
-        document.documentElement.setAttribute("data-theme", "light");
+const currentYear = (document.getElementById('currentYear').innerText = year)
+
+//Dark Theme - Light Theme;
+let checkbox = document.getElementById('checkbox')
+
+checkbox.addEventListener('change', function () {
+    if (this.checked) {
+        document.documentElement.setAttribute('data-theme', 'dark')
+    } else {
+        document.documentElement.setAttribute('data-theme', 'light')
     }
-});
+})
